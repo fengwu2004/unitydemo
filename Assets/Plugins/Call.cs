7e5735bd7bcbbfe9d225e11a7a36b4b4de6dff7f
Unknown <fengwu2006@gmail.com>
2018-03-05 17:37:08 +0800

    save

--- Assets/Plugins/Call.cs
+++ Assets/Plugins/Call.cs
@@ -5,23 +5,49 @@ public class Call : MonoBehaviour {
    
 	private AndroidJavaObject jo;
 
+	private AndroidJavaClass jc;
+
+	private AndroidJavaObject activity;
+
 	void OnGUI() {
 
         //调用显示一个文本为“Hello World!”的Toest
         if(GUI.Button(new Rect(0, 0, 200, 100), "Show Toest - Hello World!")) {
             //Unity侧调用Android侧代码
 
-			AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+			if (jc == null) {
+			
+				jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+			}
 
-			var context = jc.GetStatic<AndroidJavaObject> ("currentActivity");
+			if (activity == null) {
+
+				activity = jc.GetStatic<AndroidJavaObject> ("currentActivity");
+			}
 
 			if (jo == null) {
 
 				jo = new AndroidJavaObject ("com.yihai.ky.caotang.MyActivity");
 			}
 
-			//调用成员方法
-			jo.Call ("showMap", true, context);
+			activity.Call ("runOnUiThread", new AndroidJavaRunnable (() => {
+
+				jo.Call ("showMap", true, activity);
+			}));
+        }
+
+ 		if (GUI.Button(new Rect(100, 200, 200, 100), "光线"))
+        {
+			activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+				{
+					AndroidJavaObject Window = activity.Call<AndroidJavaObject>("getWindow");
+
+					AndroidJavaObject Attributes = Window.Call<AndroidJavaObject>("getAttributes");
+
+					Attributes.Set("screenBrightness", 0.7f);
+
+					Window.Call("setAttributes", Attributes);
+				}));
         }
     }
 }
